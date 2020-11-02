dfdd413fdf22ed6f8da5b497b28908056302f31b
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-11-02 15:52:03 +0530

    Update

--- src/main/java/com/atmecs/one_to_many/crud_operations/AddRecord.java
+++ src/main/java/com/atmecs/one_to_many/crud_operations/AddRecord.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.testng.annotations.Test;
+
 import com.atmecs.one_to_many.entity.Employee_Details;
 import com.atmecs.one_to_many.entity.Team_Details;
 
 public class AddRecord 
 {
+	@Test
 	public void addRecord() 
 	{
 
@@ -21,22 +24,20 @@ public class AddRecord
 			
 			  Transaction transaction = session.beginTransaction();
 			  
-			  Team_Details team_det = new Team_Details("SDET");
-			  
-			  Employee_Details emp_det1 = new Employee_Details("Alfin","alfin.anthonyraj@atmecs.com.com",team_det); 
-			  Employee_Details emp_det2 = new Employee_Details("Padma","padma.inba@gmail.com",team_det);
-			  Employee_Details emp_det3 = new Employee_Details("Vasu","vasu.vanu@gmail.com",team_det);
-			  
-			  Set<Employee_Details> employee_det = new HashSet<Employee_Details>();
-			  employee_det.add(emp_det1); 
-			  employee_det.add(emp_det2);
-			  employee_det.add(emp_det3);
-			  
-			  team_det.setEmployee_det(employee_det);
-			 
-			
+				Team_Details team_det = new Team_Details("SDET");
+
+				Employee_Details emp_det1 = new Employee_Details("Alfin", "alfin.anthonyraj@atmecs.com.com", team_det);
+				Employee_Details emp_det2 = new Employee_Details("Padma", "padma.inba@gmail.com", team_det);
+				Employee_Details emp_det3 = new Employee_Details("Vasu", "vasu.vanu@gmail.com", team_det);
+
+				Set<Employee_Details> employee_det = new HashSet<Employee_Details>();
+				employee_det.add(emp_det1);
+				employee_det.add(emp_det2);
+				employee_det.add(emp_det3);
+
+				team_det.setEmployee_det(employee_det);
+
 			session.save(team_det);
-			
 			transaction.commit();
 		}
 		finally 
